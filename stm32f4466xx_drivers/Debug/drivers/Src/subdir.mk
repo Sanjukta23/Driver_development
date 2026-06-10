a388ec5719fb98f3045b94ebb0a271bbf0352251
Sanjukta23 <sanjuktaa23@gmail.com>
2026-06-10 11:38:58 +0100

    Initial commit

---
+++ stm32f4466xx_drivers/Debug/drivers/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/Src/main.c 
+
+OBJS += \
+./drivers/Src/main.o 
+
+C_DEPS += \
+./drivers/Src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/Src/%.o drivers/Src/%.su drivers/Src/%.cyclo: ../drivers/Src/%.c drivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -c -I"D:/Embedded/Driver_development/stm32f4466xx_drivers/drivers/Inc" -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-drivers-2f-Src
+
+clean-drivers-2f-Src:
+	-$(RM) ./drivers/Src/main.cyclo ./drivers/Src/main.d ./drivers/Src/main.o ./drivers/Src/main.su
+
+.PHONY: clean-drivers-2f-Src
+
